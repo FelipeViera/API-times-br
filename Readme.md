f0277a49e7e7278ea284e3acc015791ccbc88755
Felipe <felipe13092005@gmail.com>
2024-05-20 11:51:09 -0300

    Divisão dos títulos de cada abrangência.

--- Readme.md
+++ Readme.md
@@ -25,7 +25,29 @@ Após isto ele vai liberar um link http. Copie o link, adicione
 
 <h2> Endpoints</h2>
 <ul>
-<li> http://seu-host/times</li>
+<li> 
+http://seu-host/times (Retorna o número de títulos estaduais, nacionais e
+continentais da atual Série A do campeonato brasileiro)
+</li>
+
+<li>
+http://seu-host/times/estaduais (Retorna apenas o número de títulos estaduais)
+</li>
+
+<li>
+http://seu-host/times/nacionais (Retorna apenas o número de títulos nacionais, como a
+Copa do Brasil, o Brasileiro e a Supercopa do Brasil)
+</li>
+
+<li>
+http://seu-host/times/continentais (Retorna apenas o número de títulos continentais, como a 
+Libertadores, a Sul-Americana e a Recopa)
+</li>
+
+<Li>
+http://seu-host/times/total (Retorna os valores de todos os endpoints anteriores)
+</li>
+
 </ul>
 
 
@@ -35,6 +57,7 @@ Após isto ele vai liberar um link http. Copie o link, adicione
 <ul>
 <li>Python</li>
 <li>Criação e integração de API RESTful</li>
+<li>Manipulação de dados de planilha Excel</li>
 </ul>
 
 
--- main.py
+++ main.py
@@ -12,69 +12,112 @@ def homepage():
 def times():
 
     # Ler o arquivo Excel`
-    tabela = pd.read_excel('Times-br.xlsx')
+    tabela = pd.read_excel('br.xlsx')
     # Conta a quantidade de elementos
     caracteres = len(tabela['Times'])
     resposta = {}
     for c in range(0, 20):
-        resposta.setdefault(tabela['Times'][c], []).append("Estaduais: " + str(tabela['estaduais'][c]))
-        resposta.setdefault(tabela['Times'][c], []).append("Nacionais: " + str(tabela['nacionais'][c]))
-        resposta.setdefault(tabela['Times'][c], []).append("Continentais: " + str(tabela['continentais'][c]))
-        resposta.setdefault(tabela['Times'][c], []).append("Total: " + str(tabela['Total'][c]))
+
+        estaduais = int(tabela['Estaduais'][c])
+        resposta.setdefault(tabela['Times'][c], []).append("Estaduais: " + str(estaduais))
+
+        nacionais = int(tabela['Copa-br'][c]) + int(tabela['Serie-A'][c]) + int(tabela['Supercopa-br'][c])
+        resposta.setdefault(tabela['Times'][c], []).append("Nacionais: " + str(nacionais))
+
+        continentais = int(tabela['Libertadores'][c]) + int(tabela['Sul-americana'][c]) + int(tabela['Recopa'][c])
+        resposta.setdefault(tabela['Times'][c], []).append("Continentais: " + str(continentais))
+
+        total = continentais + nacionais + estaduais
+        resposta.setdefault(tabela['Times'][c], []).append("Total: " + str(total))
     return jsonify(resposta)
 
 @app.route('/times/estaduais')
 def estaduais():
     # Ler o arquivo Excel`
-    tabela = pd.read_excel('Times-br.xlsx')
+    tabela = pd.read_excel('br.xlsx')
     # Conta a quantidade de elementos
     caracteres = len(tabela['Times'])
-    lista = []
     resposta = {}
     for c in range(0, 20):
-        resposta.setdefault(tabela['Times'][c], []).append(str(tabela['estaduais'][c]))
-        lista.append(tabela['estaduais'][c])
+        resposta.setdefault(tabela['Times'][c], []).append(str(tabela['Estaduais'][c]))
+
     return jsonify(resposta)
 
 @app.route('/times/nacionais')
 def nacionais():
     # Ler o arquivo Excel`
-    tabela = pd.read_excel('Times-br.xlsx')
+    tabela = pd.read_excel('br.xlsx')
     # Conta a quantidade de elementos
     caracteres = len(tabela['Times'])
     lista = []
     resposta = {}
     for c in range(0, 20):
-        resposta.setdefault(tabela['Times'][c], []).append(str(tabela['nacionais'][c]))
-        lista.append(tabela['nacionais'][c])
+        copa = int(tabela['Copa-br'][c])
+        campeonato = int(tabela['Serie-A'][c])
+        supercopa = int(tabela['Supercopa-br'][c])
+        total = supercopa + copa + campeonato
+        resposta.setdefault(tabela['Times'][c], []).append("Brasileirão: " + str(campeonato))
+        resposta.setdefault(tabela['Times'][c], []).append("Copa do Brasil: " + str(copa))
+        resposta.setdefault(tabela['Times'][c], []).append("Supercopa do Brasil: " + str(supercopa))
+        resposta.setdefault(tabela['Times'][c], []).append("Total: " + str(total))
     return jsonify(resposta)
 
 @app.route('/times/continentais')
 def continentais():
     # Ler o arquivo Excel`
-    tabela = pd.read_excel('Times-br.xlsx')
+    tabela = pd.read_excel('br.xlsx')
     # Conta a quantidade de elementos
     caracteres = len(tabela['Times'])
-    lista = []
     resposta = {}
     for c in range(0, 20):
-        resposta.setdefault(tabela['Times'][c], []).append(str(tabela['continentais'][c]))
-        lista.append(tabela['continentais'][c])
+        libertadores = int(tabela['Libertadores'][c])
+        sula = int(tabela['Sul-americana'][c])
+        recopa = int(tabela['Recopa'][c])
+        total = sula + recopa + libertadores
+        resposta.setdefault(tabela['Times'][c], []).append("Libertadores: " + str(libertadores))
+        resposta.setdefault(tabela['Times'][c], []).append("Sul-americana: " + str(sula))
+        resposta.setdefault(tabela['Times'][c], []).append("Recopa: " + str(recopa))
+        resposta.setdefault(tabela['Times'][c], []).append("Continentais: " + str(total))
     return jsonify(resposta)
 
 @app.route('/times/total')
 def total():
     # Ler o arquivo Excel`
-    tabela = pd.read_excel('Times-br.xlsx')
+    tabela = pd.read_excel('br.xlsx')
     # Conta a quantidade de elementos
     caracteres = len(tabela['Times'])
-    lista = []
     resposta = {}
+
     for c in range(0, 20):
-        resposta.setdefault(tabela['Times'][c], []).append(str(tabela['Total'][c]))
-        lista.append(tabela['Total'][c])
+        libertadores = int(tabela['Libertadores'][c])
+        sula = int(tabela['Sul-americana'][c])
+        recopa = int(tabela['Recopa'][c])
+        continentais = sula + recopa + libertadores
+
+        estaduais = int(tabela['Estaduais'][c])
+        resposta.setdefault(tabela['Times'][c], []).append("Estaduais: "+ str(estaduais))
+
+        copa = int(tabela['Copa-br'][c])
+        campeonato = int(tabela['Serie-A'][c])
+        supercopa = int(tabela['Supercopa-br'][c])
+        nacionais = supercopa + copa + campeonato
+
+        resposta.setdefault(tabela['Times'][c], []).append("Brasileirão: " + str(campeonato))
+        resposta.setdefault(tabela['Times'][c], []).append("Copa do Brasil: " + str(copa))
+        resposta.setdefault(tabela['Times'][c], []).append("Supercopa do Brasil: " + str(supercopa))
+
+
+        resposta.setdefault(tabela['Times'][c], []).append("Libertadores: " + str(libertadores))
+        resposta.setdefault(tabela['Times'][c], []).append("Sul-americana: " + str(sula))
+        resposta.setdefault(tabela['Times'][c], []).append("Recopa: " + str(recopa))
+
+        resposta.setdefault(tabela['Times'][c], []).append("Nacionais: " + str(nacionais))
+        resposta.setdefault(tabela['Times'][c], []).append("Continentais: " + str(continentais))
+
+        total = estaduais + nacionais + continentais
+
+        resposta.setdefault(tabela['Times'][c], []).append("Total: " + str(total))
+
     return jsonify(resposta)
-
-
 
 app.run(host='0.0.0.0')
